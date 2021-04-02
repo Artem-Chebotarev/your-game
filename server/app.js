9fc3d5ff6a9abd2cbaec1fb79e194f001bcc1d39
IvanShchadov <shchadov.ivan@gmail.com>
2021-04-02 12:21:48 +0300

    server has been created

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const { dbConnect } = require('../server/src/db/connect')
+const TopicModel = require('./src/models/topic.model')
+const QuestionModel = require('./src/models/question.model')
 
 const PORT = 3000
 
@@ -12,8 +14,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/api/v1/game', async (req, res) => {
+  const topics = await TopicModel.find().populate('questions')
+  console.log(topics)
+  res.json(topics)
+})
 
 app.listen(PORT, () => {
   console.log('Server has been started on port ', PORT)
 })
-
