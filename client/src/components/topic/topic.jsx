a5faf1ab00ca6017ff444a09fc6f343fea5ac1eb
Фаиг <uliazhil@gmail.com>
2021-04-02 14:28:29 +0300

    add styles

--- client/src/components/topic/topic.jsx
+++ client/src/components/topic/topic.jsx
@@ -1,73 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './style.css'
 
 export default function Topic() {
   return (
     <>
-      <div className="border border-danger d-flex justify-content-center align-items-center w-100 h-100">
-        <div className="border border-primary d-flex justify-content-center align-items-center w-100 h-100">
+      <div className="d-flex justify-content-center align-items-center w-100 h-100">
+        <div className="borderItem d-flex justify-content-center align-items-center w-100 h-100">
           game
         </div>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
         </Link>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
         </Link>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
         </Link>
       </div>
-      <div className="border border-danger d-flex justify-content-center align-items-center w-100 h-100">
-        <div className="border border-primary d-flex justify-content-center align-items-center w-100 h-100">
+      <div className="d-flex justify-content-center align-items-center w-100 h-100">
+        <div className="borderItem d-flex justify-content-center align-items-center w-100 h-100">
           game
         </div>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
         </Link>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
         </Link>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
         </Link>
       </div>
-      <div className="border border-danger d-flex justify-content-center align-items-center w-100 h-100">
-        <div className="border border-primary d-flex justify-content-center align-items-center w-100 h-100">
+      <div className="d-flex justify-content-center align-items-center w-100 h-100">
+        <div className="borderItem d-flex justify-content-center align-items-center w-100 h-100">
           game
         </div>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
         </Link>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
         </Link>
         <Link
-          className="border border-primary d-flex justify-content-center align-items-center w-100 h-100"
+          className="borderItem d-flex justify-content-center align-items-center w-100 h-100"
           to="/"
         >
           <div>100</div>
--- client/src/components/topics/topics.jsx
+++ client/src/components/topics/topics.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Topic from "../topic/topic";
+import './style.css'
 
 export default function Topics() {
   return (
-    <div className="d-flex justify-content-center align-items-center vw-100 vh-100">
-      <div className="border border-danger d-flex flex-column justify-content-center align-items-center w-75 h-75">
+    <div className="d-flex justify-content-center align-items-center vw-100 vh-100 backOfGame">
+      <div className="borderGame d-flex flex-column justify-content-center align-items-center w-75 h-75">
         <Topic />
       </div>
     </div>
